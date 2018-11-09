fa95022d0bd768965a398f8c06e188ab6262dd8e
sidinsomniac <chatterjeesid1993@gmail.com>
2018-11-09 17:38:23 +0530

    Add Mapbox API key

--- js/main.js
+++ js/main.js
@@ -78,7 +78,7 @@ initMap = () => {
         scrollWheelZoom: false
       });
   L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.jpg70?access_token={mapboxToken}', {
-    mapboxToken: '<your MAPBOX API KEY HERE>',
+    mapboxToken: 'pk.eyJ1Ijoic2lkaW5zb21uaWFjIiwiYSI6ImNqbzl5OWR5YjBmeHczcW53bzBlb29mY2cifQ.ipAKlo8lfsOA6lhjUvSyKA',
     maxZoom: 18,
     attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, ' +
       '<a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, ' +
