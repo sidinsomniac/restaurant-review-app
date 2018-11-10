057dd8bd0132a248aab0180501a217e4e529d54a
sidinsomniac <chatterjeesid1993@gmail.com>
2018-11-10 23:10:00 +0530

    Make some minor changes in format

--- js/IndexController.js
+++ js/IndexController.js
@@ -1,11 +1,10 @@
-/* eslint-disable no-console */
 if ('serviceWorker' in navigator) {
 	navigator.serviceWorker.register('./sw.js')
 		.then(function(reg) {
 			console.log('Service worker registration successful');
 		})
 		.catch(function(err) {
-			console.log('Service worker registration failed.');
+			console.log('Service worker registration failed:',err);
 		});
 } else {
 	console.log('No service worker in use');
